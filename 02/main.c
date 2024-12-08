c4fbef7f801fa322a6e053db0876e6999616a209
Matteo Golin <matteo.golin@gmail.com>
2024-12-07 19:08:56 -0500

    Solved Day02 Part 2

--- 02/main.c
+++ 02/main.c
@@ -9,7 +9,7 @@ static char buffer[BUFSIZ];
 
 #define deref(type, thing) *((type *)((thing)))
 
-bool report_safe(list_t *report);
+bool report_safe(list_t *report, bool with_dampener, size_t skip_index);
 
 int main(int argc, char **argv) {
 
@@ -28,7 +28,9 @@ int main(int argc, char **argv) {
 
     /* Parse each line (report) */
 
-    size_t total_safe = 0;
+    size_t total_pure_safe = 0;
+    size_t total_damp_safe = 0;
+
     while (!feof(puzzle)) {
 
         /* Get next line */
@@ -57,10 +59,12 @@ int main(int argc, char **argv) {
 
         /* Ensure the list meets requirements */
 
-        total_safe += report_safe(&report);
+        total_pure_safe += report_safe(&report, false, list_getlen(&report));
+        total_damp_safe += report_safe(&report, true, list_getlen(&report));
     }
 
-    printf("%lu\n", total_safe);
+    printf("%lu\n", total_pure_safe);
+    printf("%lu\n", total_damp_safe);
 
     return 0;
 }
@@ -68,9 +72,11 @@ int main(int argc, char **argv) {
 /*
  * Tests if a report is safe.
  * @param report The report to test.
+ * @param with_dampener Whether to use the Problem Dampener or not.
+ * @param skip_index An index of the element to skip. If greater than the list length, no indexes will be skipped.
  * @return True if safe, false otherwise.
  */
-bool report_safe(list_t *report) {
+bool report_safe(list_t *report, bool with_dampener, size_t skip_index) {
 
     /* Any report that is less than two numbers cannot be increasing or decreasing */
 
@@ -78,40 +84,85 @@ bool report_safe(list_t *report) {
         return false;
     }
 
-    /* Start by grabbing the first two elements to see if the list is increasing or decreasing */
+    /* Check if we are increasing or decreasing on average */
 
     int last_num = deref(int, list_getindex(report, 0));
-    int cur_num = deref(int, list_getindex(report, 1));
+    int cur_num;
+    unsigned int num_incr = 0;
 
-    bool increasing = false;
-
-    if (last_num - cur_num > 0) {
-        increasing = true;
+    for (size_t i = 1; i < list_getlen(report); i++) {
+        cur_num = deref(int, list_getindex(report, i));
+        if (cur_num - last_num > 0) {
+            num_incr++;
+        }
+        last_num = cur_num;
     }
 
-    /* Make sure that all numbers meet the criteria. Loop starts at 1 to skip over first index. */
+    /* If most pairs have an increasing pattern, then we assume increase for this list */
+
+    bool increasing = num_incr > (list_getlen(report) - num_incr);
+
+    /* If the first index should be skipped, lets offset the start position */
+
+    size_t start_position = 0;
+    if (skip_index == 0) {
+        start_position++;
+    }
+
+    /* Iterate over the list and check each pair. Start one ahead of start position. */
 
     int diff;
-    for (size_t i = 1; i < list_getlen(report); i++) {
+    bool safe_round;
+    last_num = deref(int, list_getindex(report, start_position));
+    for (size_t i = start_position + 1; i < list_getlen(report); i++) {
 
+        /* Skip the skip index before any calculations */
+
+        if (i == skip_index) {
+            continue;
+        }
+
+        safe_round = true; /* Each round starts safe */
         cur_num = deref(int, list_getindex(report, i));
         diff = last_num - cur_num; /* Calculate difference */
 
         /* Check that we match increasing/decreasing pattern */
 
-        if ((increasing && diff < 0) || (!increasing && diff > 0)) {
-            return false;
-        }
+        safe_round = safe_round && ((increasing && diff < 0) || (!increasing && diff > 0));
 
         /* Adjacent levels differ by at least one and at most three */
 
-        if (abs(diff) > 3 || abs(diff) < 1) {
+        safe_round = safe_round && ((abs(diff) >= 1) && (abs(diff) <= 3));
+
+        /* Unsafe round with no dampener triggers a failure. */
+
+        if (!safe_round && !with_dampener) {
             return false;
         }
 
-        /* Record the last number */
+        /* Safe round gets a pass */
 
-        last_num = cur_num;
+        if (safe_round) {
+            last_num = cur_num;
+            continue;
+        }
+
+        /* If we're here, the dampener was on and we had an unsafe round.
+         * The problem can either be resolved by removing this element, or the previous element.
+         */
+
+        with_dampener = false; /* Dampener one time use is over */
+
+        /* Skip this element */
+
+        if (report_safe(report, false, i)) {
+            return true;
+        }
+
+        /* Skip previous element if it is not out of range. If not safe, then this report cannot be saved with the
+         * Problem Damper. */
+
+        return (i != 0) && report_safe(report, false, i - 1);
     }
 
     return true;
