f459270c025fb90eafc0120219fb09f8bd2107ff
Matteo Golin <matteo.golin@gmail.com>
2024-12-12 20:37:46 -0500

    Naive solution that works for sample input. Hangs on iteration eight of the real input though.

--- 06/main.c
+++ 06/main.c
@@ -8,8 +8,10 @@
 #include "../common/set.h"
 
 #define deref(type, thing) (*((type *)(thing)))
+
 #define GUARD_CHAR '^'
 #define FREESPACE '.'
+#define OBSTACLE 'O'
 
 typedef struct {
     int x;
@@ -46,6 +48,7 @@ static int out_of_bounds(coord_t coords, size_t xlen, size_t ylen) {
 static coord_t coord_add(coord_t a, coord_t b) { return (coord_t){.x = a.x + b.x, .y = a.y + b.y}; }
 
 void record_visited(guard_t guard, list_t *grid, size_t xlen, size_t ylen, set_t *visited);
+bool has_loop(guard_t guard, list_t *grid, size_t xlen, size_t ylen);
 
 int main(int argc, char **argv) {
 
@@ -109,15 +112,99 @@ int main(int argc, char **argv) {
 
     set_t visited;
     set_create(&visited, NULL, BUFSIZ, sizeof(coord_t));
+
     record_visited(guard, &grid, xlen, ylen, &visited);
 
     printf("%lu\n", set_len(&visited));
+
+    /* Now go through all of the spots that the guard naturally visits, and select one to put an obstacle in */
+
+    char obstacle = OBSTACLE;
+    size_t loops = 0;
+
+    coord_t *loc;
+    size_t i = 0;
+    size_t count = 0;
+    while (set_iter(&visited, &i, (void *)&loc) != NULL) {
+
+        count++;
+        printf("Iteration %lu\n", count);
+        /* Can't put an obstacle where the guard is standing! */
+
+        if (loc->x == guard.pos.x && loc->y == guard.pos.y) continue;
+
+        list_setindex(&grid, loc->y * ylen + loc->x, &obstacle);  /* Place an obstacle at this spot */
+        if (has_loop(guard, &grid, xlen, ylen)) loops++;          /* We found a loop due to this obstacle! */
+        list_setindex(&grid, loc->y * ylen + loc->x, &freespace); /* Remove the obstacle for the next go-round */
+    }
+
+    printf("%lu\n", loops);
 
     /* Close input */
 
     list_destroy(&grid);
     set_destroy(&visited);
     fclose(puzzle);
+}
+
+/* Detects if the guard will move in a loop on this grid.
+ * @param guard The guard with its initial starting position and direction
+ * @param grid The map
+ * @param xlen The number of columns in the map
+ * @param ylen The number of rows in the map
+ * @return True if the guard will loop, false if not
+ */
+bool has_loop(guard_t guard, list_t *grid, size_t xlen, size_t ylen) {
+
+    /* Local copy of visited locations for this run */
+
+    set_t visited;
+    set_create(&visited, NULL, 2048, sizeof(guard_t));
+
+    set_add(&visited, &guard.pos); /* Record start position */
+
+    /* Start state machine logic */
+
+    coord_t new_pos;
+
+    for (;;) {
+
+        new_pos = coord_add(guard.pos, MOVE[guard.dir]);
+
+        /* If the guard went out of bounds, we're done */
+
+        if (out_of_bounds(new_pos, xlen, ylen)) {
+            break;
+        }
+
+        /* If the guard would hit an object, turn 90 degrees right and continue forward */
+
+        if (deref(char, list_getindex(grid, new_pos.y * ylen + new_pos.x)) != FREESPACE) {
+            guard.dir = RIGHT_TURN[guard.dir];
+            continue;
+        }
+
+        /* If we're in the grid and didn't hit anything, we're here. It's safe to update the current position. */
+
+        guard.pos = new_pos;
+
+        /* Check if we've been in this state before.
+         * If so, we're in a loop!
+         * If not, record the state and continue.
+         */
+
+        if (set_contains(&visited, &guard)) {
+            set_destroy(&visited);
+            return true;
+        }
+
+        set_add(&visited, &guard); /* Record this state */
+    }
+
+    /* We exited the loop because the guard tried to go out of bounds, so no loop here */
+
+    set_destroy(&visited);
+    return false;
 }
 
 /* Records all the locations visited by the guard during its journey.
@@ -146,7 +233,7 @@ void record_visited(guard_t guard, list_t *grid, size_t xlen, size_t ylen, set_t
 
         /* If the guard would hit an object, turn 90 degrees right and continue forward */
 
-        if (deref(char, list_getindex(grid, new_pos.y * ylen + new_pos.x)) == '#') {
+        if (deref(char, list_getindex(grid, new_pos.y * ylen + new_pos.x)) != FREESPACE) {
             guard.dir = RIGHT_TURN[guard.dir];
             continue;
         }
@@ -154,6 +241,6 @@ void record_visited(guard_t guard, list_t *grid, size_t xlen, size_t ylen, set_t
         /* If we're in the grid and didn't hit anything, we're here. Record the position */
 
         guard.pos = new_pos;
-        set_add(visited, &guard.pos); /* Record start position */
+        set_add(visited, &guard.pos); /* Record this position */
     }
 }
